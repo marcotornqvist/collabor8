922624fd4177c55cfcd7d95dd993f570a8a8f2ff
marcotornqvist <marco.tornqvist@gmail.com>
2021-07-27 17:37:37 +0300

    Test commit

--- backend/index.ts
+++ backend/index.ts
@@ -3,18 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     name: "Alice",
-  //     email: "alice@prisma.io",
-  //     // posts: {
-  //     //   create: { title: "Hello World" },
-  //     // },
-  //     profile: {
-  //       create: { bio: "I like turtles" },
-  //     },
-  //   },
-  // });
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      // posts: {
+      //   create: { title: "Hello World" },
+      // },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
 
   const allUsers = await prisma.user.findMany({
     include: {
