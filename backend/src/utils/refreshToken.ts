b6a7b8e153fd08a952f9ac9a051f104b818a1c84
marcotornqvist <marco.tornqvist@gmail.com>
2021-10-09 16:47:30 +0300

    Changed cookies to lax

--- backend/src/utils/refreshToken.ts
+++ backend/src/utils/refreshToken.ts
@@ -43,8 +43,6 @@ export const sendRefreshToken = async (res: Response, token: string) => {
     httpOnly: true,
     path: "/refresh_token",
     maxAge: 1000 * 3600 * 24 * 30 * 1, // 1 month
-    sameSite: "none",
-    secure: true,
   });
 };
 
